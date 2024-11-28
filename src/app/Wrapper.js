57487030c7ce73a1783e496b1946d9d0bf82b69c
Sagar Johari <joharisagar53@gmail.com>
2024-11-29 01:08:51 +0530

    changes by sagar

---
+++ src/app/Wrapper.js
@@ -0,0 +1,11 @@
+// src/app/Wrapper.js
+export const metadata = {
+    title: "Sagar Johari | Front-End Developer",
+    description:
+      "Showcasing the portfolio of Sagar Johari, a skilled front-end developer with expertise in React.js, GSAP, WordPress, and Shopify. Explore innovative web solutions, animations, and custom plugins designed to elevate your digital presence.",
+  };
+  
+  export default function Wrapper({ children }) {
+    return <>{children}</>;
+  }
+  
---
+++ src/app/works/page.js
@@ -0,0 +1,9 @@
+export default function Works() {
+    return (
+      <div>
+        <h1>Works Page</h1>
+        <p>Check out my works here.</p>
+      </div>
+    );
+  }
+      
